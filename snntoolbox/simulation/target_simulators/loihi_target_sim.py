b1d1439210395a45af0b7f2d2c449a5629f92e55
Biagio Montaruli <biagio.hkr@gmail.com>
2020-09-18 14:16:37 +0200

    Update the name of NxSDK folder that contains the modules for DSNN and NxTF.

--- snntoolbox/simulation/target_simulators/loihi_target_sim.py
+++ snntoolbox/simulation/target_simulators/loihi_target_sim.py
@@ -7,4 +7,4 @@ Currently, this code is part of the nxsdk, so we redirect there.
 """
 
 # noinspection PyUnresolvedReferences
-from nxsdk_modules.snntoolbox.nx_backend import SNN
+from nxsdk_modules_ncl.snntoolbox.nx_backend import SNN
